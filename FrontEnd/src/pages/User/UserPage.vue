9b86acfc2d41d806f65a3e89ab4bdbbb8ef9b0f8
sue06004 <sue06004@naver.com>
2024-08-08 14:02:55 +0900

    [Refactor] 일반 유저 페이지 Component 리팩토링

---
+++ FrontEnd/src/pages/User/UserPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="wrapper">
+    <HeaderComponent></HeaderComponent>
+    <router-view></router-view>
+    <FooterComponent></FooterComponent>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/User/HeaderComponent.vue';
+import FooterComponent from '@/components/User/FooterComponent.vue';
+export default {
+    name: "UserPage",
+    data(){
+        return {
+            isLoading:true
+        }
+    },
+    components: {
+        HeaderComponent,
+        FooterComponent
+    },
+    methods:{
+        handleIsLoading(isLoading){
+            this.isLoading = isLoading;
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
