04efd0e0442b0c5f95d4efeed656d10409b0fa1a
sue06004 <sue06004@naver.com>
2024-08-12 00:45:14 +0900

    [Fix] 카카오 로그인 링크 주소 변경

--- FrontEnd/src/pages/Common/LoginPage.vue
+++ FrontEnd/src/pages/Common/LoginPage.vue
@@ -76,7 +76,7 @@
         <div id="snsLogin" class="snsLogin" v-if="activeOption === 'user'" style="display: flex;">
           <ul>
             <li id="kakao_li">
-              <a href="/api/oauth2/authorization/kakao" class="kakao" id="openid_kakao">
+              <a href="https://api.fourtrees.kro.kr/oauth2/authorization/kakao" class="kakao" id="openid_kakao">
                 <span>카카오톡</span>
               </a>
             </li>
